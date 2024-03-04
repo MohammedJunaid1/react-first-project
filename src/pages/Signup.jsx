98a18186e8fc3d00ca9dfa57a7a30e266afd42cf
engjunaidweb <junaidbarkati123@gmail.com>
2024-03-04 14:21:31 +0530

    sign page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,56 @@
+function Signup() {
+  return (
+    <>
+      <div className=" back h-screen flex justify-center items-center">
+        <h1 className="h1 flex-none text-8xl mr-20">WalkWise</h1>
+        <div className=" container w-1/3 h-1/3 shadow-2xl">
+          <form className=" bg-red-50 shadow-md rounded px-8 pt-6 pb-8 mb-4  ">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Username or Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="username"
+                type="text"
+                placeholder="Username or Email"
+              />
+            </div>
+            <div className="mb-6">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                id="password"
+                type="password"
+                placeholder="******************"
+              />
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Sign In
+              </button>
+              <a
+                className="inline-block align-baseline font-bold text-sm text-red-500 hover:text-red-800"
+                href="#"
+              >
+                Forgot Password?
+              </a>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+export default Signup;
