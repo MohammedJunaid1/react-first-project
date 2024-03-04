7c491fb66876134afc1359c1cd8723446c7d9fe6
MohdSaquib114 <msmsaq114@gmail.com>
2024-03-04 21:59:59 +0530

    Created signup page

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,56 +1,43 @@
+import { Link } from "react-router-dom";
+import { LabelledInput } from "../components/LabelledInput";
+
 function Signup() {
   return (
     <>
       <div className=" back h-screen flex justify-center items-center">
         <h1 className="h1 flex-none text-8xl mr-20">WalkWise</h1>
-        <div className=" container w-1/3 h-1/3 shadow-2xl">
-          <form className=" bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4  ">
+        <div className=" container w-1/3 h-1/3 shadow-2xl ">
+          <form className=" bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4  rounded-2xl ">
+            <h1 className=" text-center text-3xl font-semibold text-slate-700 mb-4">Create an Account</h1>
+            <p className="text-center text-md text-slate-600">Already have an account?
+              <Link className="underline" to={"/signin"}>Login</Link>
+            </p>
             <div className="mb-4">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="username"
-              >
-                Username or Email
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                id="username"
-                type="text"
-                placeholder="Username or Email"
-              />
+              <LabelledInput label={"Full Name"} placeholder={"Type your name..."} type={"text"} />
+            </div>
+            <div className="mb-4">
+              <LabelledInput label={"Username or Email"} placeholder={"Username or Email"} type={"text"} />
             </div>
             <div className="mb-6">
-              <label
-                className="block text-gray-700 text-sm font-bold mb-2"
-                htmlFor="password"
-              >
-                Password
-              </label>
-              <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-                id="password"
-                type="password"
-                placeholder="******************"
-              />
+              <LabelledInput label={"Set Password"} placeholder={"******************"} type="password" />
             </div>
             <div className="flex items-center justify-between">
               <button
                 className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
                 type="submit"
               >
-                Sign In
+                Sign Up
               </button>
-              <a
-                className="inline-block align-baseline font-bold text-sm text-red-500 hover:text-red-800"
-                href="#"
-              >
-                Forgot Password?
-              </a>
+
             </div>
+
           </form>
+
         </div>
       </div>
     </>
   );
 }
+
+
 export default Signup;
