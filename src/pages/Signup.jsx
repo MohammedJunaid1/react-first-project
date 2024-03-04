37bc3c4d479810b18d9a60db940ab3e35ac4d0a9
engjunaidweb <junaidbarkati123@gmail.com>
2024-03-04 22:08:26 +0530

    junaid

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,16 +4,16 @@ function Signup() {
       <div className=" back h-screen flex justify-center items-center">
         <h1 className="h1 flex-none text-8xl mr-20">WalkWise</h1>
         <div className=" container w-1/3 h-1/3 shadow-2xl">
-          <form className=" bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4  ">
+          <form className="bg-opacity-10 bg-gray-100 shadow-md rounded px-8 pt-6 pb-8 mb-4  ">
             <div className="mb-4">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-red-500 text-sm font-bold mb-2"
                 htmlFor="username"
               >
                 Username or Email
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
                 id="username"
                 type="text"
                 placeholder="Username or Email"
@@ -21,13 +21,13 @@ function Signup() {
             </div>
             <div className="mb-6">
               <label
-                className="block text-gray-700 text-sm font-bold mb-2"
+                className="block text-red-500 text-sm font-bold mb-2"
                 htmlFor="password"
               >
                 Password
               </label>
               <input
-                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-800 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
                 type="password"
                 placeholder="******************"
